82cd7941d1dd28fa14a67a6e6b912301f1a5e1a1
Stian Thorgersen <stianst@gmail.com>
2026-01-29 14:15:45 +0100

    Make sure registration tokens are verified before processing registration
    Fixes CVE-2026-1529
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/organization/utils/Organizations.java
+++ services/src/main/java/org/keycloak/organization/utils/Organizations.java
@@ -35,6 +35,8 @@ import org.keycloak.authentication.actiontoken.inviteorg.InviteOrgActionToken;
 import org.keycloak.common.Profile;
 import org.keycloak.common.Profile.Feature;
 import org.keycloak.common.VerificationException;
+import org.keycloak.crypto.SignatureProvider;
+import org.keycloak.crypto.SignatureVerifierContext;
 import org.keycloak.http.HttpRequest;
 import org.keycloak.models.Constants;
 import org.keycloak.models.FederatedIdentityModel;
@@ -50,6 +52,7 @@ import org.keycloak.models.UserModel;
 import org.keycloak.organization.OrganizationProvider;
 import org.keycloak.organization.protocol.mappers.oidc.OrganizationScope;
 import org.keycloak.services.ErrorResponse;
+import org.keycloak.services.Urls;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
 public class Organizations {
@@ -148,7 +151,7 @@ public class Organizations {
         }
     }
 
-    public static InviteOrgActionToken parseInvitationToken(HttpRequest request) throws VerificationException {
+    public static InviteOrgActionToken parseInvitationToken(KeycloakSession session, HttpRequest request) throws VerificationException {
         MultivaluedMap<String, String> queryParameters = request.getUri().getQueryParameters();
         String tokenFromQuery = queryParameters.getFirst(Constants.TOKEN);
 
@@ -156,7 +159,16 @@ public class Organizations {
             return null;
         }
 
-        return TokenVerifier.create(tokenFromQuery, InviteOrgActionToken.class).getToken();
+        KeycloakContext context = session.getContext();
+        RealmModel realm = session.getContext().getRealm();
+        TokenVerifier<InviteOrgActionToken> verifier = TokenVerifier.create(tokenFromQuery, InviteOrgActionToken.class)
+                .withChecks(TokenVerifier.IS_ACTIVE,
+                        new TokenVerifier.RealmUrlCheck(Urls.realmIssuer(context.getUri().getBaseUri(), realm.getName())));
+
+        SignatureVerifierContext verifierContext = session.getProvider(SignatureProvider.class, verifier.getHeader().getAlgorithm().name()).verifier(verifier.getHeader().getKeyId());
+        verifier.verifierContext(verifierContext);
+
+        return verifier.verify().getToken();
     }
 
     public static String getEmailDomain(String email) {
